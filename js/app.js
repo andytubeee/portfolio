58791d6283c71418130c0c7050bc00df168ad941
andytubeee <andy.tubeee@gmail.com>
2020-08-24 21:54:44 -0400

    copy email

--- js/app.js
+++ js/app.js
@@ -1,8 +1,6 @@
 $(() => {
   // Page onload
   document.querySelector('body').onload = () => {
-    // document.getElementById('alwaysFullscreen').style.height =
-    //   $(window).height() + 'px';
   };
   // Learn more button click
   $('#learnmoreBtn').click(() => {
@@ -10,21 +8,44 @@ $(() => {
     console.log($('header.masthead').height());
   });
 
+  // Tooltip
+  $('[data-toggle="tooltip"]').tooltip();
+
+
+  // School Website
   $('.school-txt').click(() => window.open('https://jam.wrdsb.ca/', '_blank'));
 
+  // Skills tooltip
   var mySkills = document.querySelectorAll('li.skillListItem');
   mySkills.forEach((skills) => {
-    skills.onmouseenter = () => console.log(skills.innerHTML);
+    // skills.onmouseenter = () => console.log(skills.innerHTML);
   });
 
-  var contactBtns = document.querySelectorAll('div.contact-btns');
-
-  contactBtns.forEach((btn) => {
-    btn.onclick = () => btn.style.border = 'none';
-  });
-
+  // Contact me scroll
   $('a.contactme-txt').click(() => {
     $('html, body').animate({ scrollTop: $(document).height() }, 800);
     return false;
+  });
+
+  const copyToClipboard = str => {
+    const el = document.createElement('textarea');
+    el.value = str;
+    document.body.appendChild(el);
+    el.select();
+    document.execCommand('copy');
+    document.body.removeChild(el);
+  };
+
+  // Contact me onclicks
+  $('button.linkedin-color').click(() => window.open('https://www.linkedin.com/in/andrew-yang-7a9402198/', '_blank'));
+  $('button.github-btn').click(() => window.open('https://www.github.com/andytubeee', '_blank'));
+  $('button.email-color').click(() => {
+    var $myEmail = "andy.tubeee@gmail.com";
+    try {
+      copyToClipboard($myEmail);
+      alert("My email is copied to your clipboard. :D");
+    } catch (err) {
+      alert("Failed: " + err);
+    }
   });
 });
