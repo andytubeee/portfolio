ad5f92ed31ecd93a2491232af97b9dfdac2b2986
andytubeee <andy.tubeee@gmail.com>
2020-09-01 15:51:47 -0400

    added bulma css

--- projects/projects.js
+++ projects/projects.js
@@ -19,4 +19,7 @@ $(() => {
 
     // Refresh the page when user clicks project text again on the same page
     $('a.projects-txt').click(() => location.reload());
+
+    // Project Devpost Links
+    $('img.tc-img').click(() => window.open("https://devpost.com/software/treecompanion", "_blank"));
 });
