dfe037e348ba115b5022882500d21bcc328d7417
andytubeee <andy.tubeee@gmail.com>
2020-09-01 16:43:23 -0400

    added two projects with sweetalert library

--- projects/projects.js
+++ projects/projects.js
@@ -22,4 +22,21 @@ $(() => {
 
     // Project Devpost Links
     $('img.tc-img').click(() => window.open("https://devpost.com/software/treecompanion", "_blank"));
+
+    //Tree Companion
+    $('button.tc-demo').click(() => window.open("https://www.youtube.com/watch?v=QqRB1BK4_zk", "_blank"));
+    $('button.tc-repo').click(() => window.open("http://www.github.com/andytubeee/Tree_Companion", "_blank"));
+
+    // VegLife
+    $('button.vgl-demo').click(() => window.open("https://www.youtube.com/watch?v=DRxRsoKW0oE", "_blank"));
+    $('button.vgl-repo').click(() => {
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Unfortunately, the repository is deleted on GitHub, sorry for your inconvenience.',
+            footer: 'Please check out my other projects.'
+        });
+    });
+
+
 });
